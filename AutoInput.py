274d5a21554ba75d725d13dde8d07aa060ac26d7
JuNM-0418 <goojsm13@naver.com>
2021-05-25 19:37:12 +0900

    Update AutoInput.py
    함수로 변경  및 변수 이름 변경

--- AutoInput.py
+++ AutoInput.py
@@ -7,6 +7,21 @@ from tqdm import trange
 
 
 
+# 이전 행(Row)을 받고 다음 사진이 삽입될 행(Row)의 넘버를 반환해주는 함수
+def Next_Location(location_Row):
+    location_Row = location_Row + 32
+    return(location_Row)
+
+
+# 행, 열, 사진경로, 폴더이름, 사진 번호를 받고 사진을 해당위치에 삽입 및 다음 사진 넘버를 반환 해주는 함수
+def Input_Image(location_Col, location_Row, Path, Building, m, Image_Cycle):
+    location = location_Col + str(location_Row)
+    rng = ws.Range(location) 
+    Image_Path = Path+"\\" + str(Building[m]) + "\\" + str(Image_Cycle) + ".jpg" 
+    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28)
+    Image_Cycle = Image_Cycle + 1
+    return(Image_Cycle) 
+
 
 # 파일 이름, 시트이름 등등을 입력받음
 file_name = input("파일 이름을 입력하세요 : ")
@@ -17,179 +32,81 @@ for n in range(0,int(Building_Num),1):
     Building[n] = input("동의 이름을 입력하세요 : ")
 
 
-
-
-# 실행파일이 있는 곳에 있는 엑셀 양식 파일, 엑셀 시트 열기
+# 실행파일이 있는 위치에 있는 엑셀 양식 파일, 엑셀 시트 열기
 Path = os.getcwd()
 excel = win32.gencache.EnsureDispatch('Excel.Application')
-wb = excel.Workbooks.Add(Path+"\\"+file_name+".xlsx")
+wb = excel.Workbooks.Add(Path + "\\" + file_name + ".xlsx")
 ws = wb.Sheets(Sheets_name)
 
 
-
-# 사진 삽입 위치 초기값 및 초기화
-location1=3
-location2=13
-location3=23
-location="B"
-locate1=0
-locate2=0
-locate3=0
+location_Row_1 = 3 # 페이지 내 첫번째 시작 행
+location_Row_2 = 13 # 페이지 내 두번쨰 시작 행
+location_Row_3 = 23 # 페이지 내 세번째 시작 행
+location_Col_1 = "B" # 고정 열
+Image_Cycle = 1   # 각 동의 폴더에서 넣을 사진의 순서
 
 
-# 동의 개수만큼 반복
-for m in range(0,int(Building_Num),1):
+for m in range(0, int(Building_Num), 1):
     
 
-
     # 각 동의 폴더에서 .jpg로 끝나는 파일 리스트 만들기
-    Path = os.getcwd()
-    file_list = os.listdir(Path + "\\"+ str(Building[m]))
+    file_list = os.listdir(Path + "\\" + str(Building[m]))
     file_list_jpg = [file for file in file_list if file.endswith(".jpg") or file.endswith("JPG")]   
 
 
-    Image_cycle=1   # 각 동의 폴더에서 넣을 사진의 순서
-    Image_Num = len(file_list_jpg)  # .jpg로 끝나는 사진들의 리스트 개수
+    Image_Cycle = 1 # 사진번호 초기화
+    Image_Num = len(file_list_jpg)  # .jpg로 끝나는 사진의 개수
 
 
 
 
-    NUM=Image_Num // 3 # 사진이 무조곡 다 들어가는 페이지수
-    NUM_1=3-(Image_Num % 3) # 마지막 페이지에서 모자란 사진 숫자
+    NUM=Image_Num // 3 # 사진이 무조건 다 들어가는 페이지수
+    NUM_1 = 3 - (Image_Num % 3) # 마지막 페이지에서 모자란 사진 숫자
     if(Image_Num % 3 > 0):
-        NUM=NUM+1 # 다들어가지 않고 남는 사진이 있으면 남는 사진이 들어간는 페이지 수 추가
+        NUM = NUM +1 # 다들어가지 않고 남는 사진이 있으면 남는 사진이 들어간는 페이지 수 추가
         
         
-        #사진 갯수가 3의 배수가 아니면 마지막 사진을 그다음번호로 복사함
-        for k in range(1,NUM_1+1,1):
-            shutil.copyfile(Path+"\\" + str(Building[m]) + "\\" +str(Image_Num)+".jpg", Path+"\\"+str(Building[m]) + "\\" +str(Image_Num+k)+".jpg") 
+        #사진 갯수가 3의 배수가 아니면 마지막 사진을 그다음번호로 복사함 
+        for k in range(1, NUM_1+1, 1):
+            shutil.copyfile(Path + "\\" + str(Building[m]) + "\\" + str(Image_Num) + ".jpg", Path + "\\" + str(Building[m]) + "\\" + str(Image_Num+k) + ".jpg") 
 
 
         # 사진 넣기
-        for i in trange(0,int(NUM),1):
+        for i in trange(0, int(NUM), 1):
             time.sleep(0.1)
-            if(i==0):
-                try:
-                    locate1=location+str(location1)
-                    rng = ws.Range(locate1) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
+            try:
+                Image_Cycle = Input_Image(location_Col_1, location_Row_1, Path, Building, m, Image_Cycle)
+                Image_Cycle = Input_Image(location_Col_1, location_Row_2, Path, Building, m, Image_Cycle)
+                Image_Cycle = Input_Image(location_Col_1, location_Row_3, Path, Building, m, Image_Cycle)
 
+            except:
+                print(str(Building[m]) + " " + str(Image_Cycle) + ".jpg 사진이 없습니다.")
+                break
+            
 
-                
-                    locate2=location+str(location2)
-                    rng = ws.Range(locate2) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-
-
-
-                    locate3=location+str(location3)
-                    rng = ws.Range(locate3) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27) 
-                    Image_cycle=Image_cycle+1
-                except:
-                    print(str(Building[m])+" "+str(Image_cycle)+".jpg 사진이 없습니다.")
-                    break
-
-            else:
-                
-                try:
-                    locate1=location+str(location1)
-                    rng = ws.Range(locate1) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)                 
-                    Image_cycle=Image_cycle+1
-
-
-
-       
-                    locate2=location+str(location2)
-                    rng = ws.Range(locate2) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-
-
-
-                    locate3=location+str(location3)
-                    rng = ws.Range(locate3) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-                except:
-                    print(str(Building[m])+" "+str(Image_cycle)+".jpg 사진이 없습니다.")
-                    break
-            location1=location1+32
-            location2=location2+32
-            location3=location3+32
-
-
+            location_Row_1 = Next_Location(location_Row_1)
+            location_Row_2 = Next_Location(location_Row_2)
+            location_Row_3 = Next_Location(location_Row_3)
 
 
     else:
-        for i in trange(0,int(NUM),1):
+        for i in trange(0, int(NUM) ,1):
             time.sleep(0.1)
-            if(i==0):
-                try:
-                    locate1=location+str(location1)
-                    rng = ws.Range(locate1) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
+            try:
+                Image_Cycle = Input_Image(location_Col_1, location_Row_1, Path, Building, m, Image_Cycle)
+                Image_Cycle = Input_Image(location_Col_1, location_Row_2, Path, Building, m, Image_Cycle)
+                Image_Cycle = Input_Image(location_Col_1, location_Row_3, Path, Building, m, Image_Cycle)
+            except:
+                print(str(Building[m]) + " " + str(Image_Cycle) + ".jpg 사진이 없습니다.")
+                break
 
 
+            location_Row_1 = Next_Location(location_Row_1)
+            location_Row_2 = Next_Location(location_Row_2)
+            location_Row_3 = Next_Location(location_Row_3)
 
-                    locate2=location+str(location2)
-                    rng = ws.Range(locate2) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-
-
-                    locate3=location+str(location3)
-                    rng = ws.Range(locate3) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-                except:
-                    print(str(Building[m])+" "+str(Image_cycle)+".jpg 사진이 없습니다.")
-                    break
-
-
-            else:
-                try:
-                    locate1=location+str(location1)
-                    rng = ws.Range(locate1) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-
-         
-                    locate2=location+str(location2)
-                    rng = ws.Range(locate2) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-
-
-          
-                    locate3=location+str(location3)
-                    rng = ws.Range(locate3) 
-                    Image_Path = Path+"\\" + str(Building[m]) + "\\" +str(Image_cycle) +".jpg" 
-                    image = ws.Shapes.AddPicture(Image_Path, False,True, rng.Left, rng.Top, 247.68, 184.28) #조사표 사진 크기 -> 완료(2021.04.27)
-                    Image_cycle=Image_cycle+1
-                except:
-                    print(str(Building[m])+" "+str(Image_cycle)+".jpg 사진이 없습니다.")
-                    break
-        
-            location1=location1+32
-            location2=location2+32
-            location3=location3+32
-    print(Building[m]+" 사진 삽입 완료.")
+            
+    print(Building[m] + " 사진 삽입 완료.")
 
 
 print("모든 사진 삽입 완료.")

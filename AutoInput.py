0f2d060c299beb61345fb5e03a01c0e72cc55fc6
JuNM-0418 <goojsm13@naver.com>
2021-05-25 19:39:43 +0900

    Update AutoInput.py
    사진 번호 확인 수식 삽입 함수, 내용 합성 수식 삽입 함수 추가

--- AutoInput.py
+++ AutoInput.py
@@ -6,6 +6,18 @@ from tqdm import tqdm
 from tqdm import trange
 
 
+# 설명번호와 사진번호가 맞는지 확인해주는 엑셀수식을 삽입해주는 함수
+def Check_ImageNum(location_Row, location_Col):
+    ws.Cells(int(location_Row)+1, 21).Value = "=IF(MID(O"+str(int(location_Row)+1)+",SEARCH(\".\",O"+str(int(location_Row)+1)+",1),3)=MID(W"+str(int(location_Row)+8)+",SEARCH(\".\",W"+str(int(location_Row)+8)+",1),3),\"\",\"번호확인\")" 
+    ws.Cells(int(location_Row)+1, 21).Font.Color = -16776961
+    return()
+
+
+# 설명부분 내용을 합성해주는 엑셀수식을 삽입해주는 함수
+def Combine_Explanation(location_Row, location_Col):
+    ws.Cells(int(location_Row)+4, 15).Value = "="+ location_Col + str(int(location_Row) + 8) + "&"+"\"균열\"" 
+    return()
+
 
 # 이전 행(Row)을 받고 다음 사진이 삽입될 행(Row)의 넘버를 반환해주는 함수
 def Next_Location(location_Row):
@@ -25,7 +37,8 @@ def Input_Image(location_Col, location_Row, Path, Building, m, Image_Cycle):
 
 # 파일 이름, 시트이름 등등을 입력받음
 file_name = input("파일 이름을 입력하세요 : ")
-Sheets_name = input("시트 이름을 입력하세요 : ")
+Survey_Sheets_name = input("조사표 시트 이름을 입력하세요 : ")
+Sheets_name = input("조사사진 시트 이름을 입력하세요 : ")
 Building_Num = input("동의 개수를 입력하세요 : ")
 Building = [0 for z in range(int(Building_Num))]
 for n in range(0,int(Building_Num),1):
@@ -37,12 +50,15 @@ Path = os.getcwd()
 excel = win32.gencache.EnsureDispatch('Excel.Application')
 wb = excel.Workbooks.Add(Path + "\\" + file_name + ".xlsx")
 ws = wb.Sheets(Sheets_name)
+ws_Survey = wb.Sheets(Survey_Sheets_name)
 
 
 location_Row_1 = 3 # 페이지 내 첫번째 시작 행
 location_Row_2 = 13 # 페이지 내 두번쨰 시작 행
 location_Row_3 = 23 # 페이지 내 세번째 시작 행
 location_Col_1 = "B" # 고정 열
+location_Col_2 = "U" # 번호확인 함수 위치
+location_Col_3 = "O" # 설명확인 함수 위치
 Image_Cycle = 1   # 각 동의 폴더에서 넣을 사진의 순서
 
 
@@ -75,14 +91,23 @@ for m in range(0, int(Building_Num), 1):
         for i in trange(0, int(NUM), 1):
             time.sleep(0.1)
             try:
+                # 사진 삽입
                 Image_Cycle = Input_Image(location_Col_1, location_Row_1, Path, Building, m, Image_Cycle)
                 Image_Cycle = Input_Image(location_Col_1, location_Row_2, Path, Building, m, Image_Cycle)
                 Image_Cycle = Input_Image(location_Col_1, location_Row_3, Path, Building, m, Image_Cycle)
 
+
             except:
                 print(str(Building[m]) + " " + str(Image_Cycle) + ".jpg 사진이 없습니다.")
                 break
-            
+            # 엑셀 수식 삽입
+            Combine_Explanation(location_Row_1,  location_Col_2)
+            Combine_Explanation(location_Row_2,  location_Col_2)
+            Combine_Explanation(location_Row_3,  location_Col_2)
+            Check_ImageNum(location_Row_1, location_Col_2)
+            Check_ImageNum(location_Row_2, location_Col_2)
+            Check_ImageNum(location_Row_3, location_Col_2)
+
 
             location_Row_1 = Next_Location(location_Row_1)
             location_Row_2 = Next_Location(location_Row_2)
@@ -93,12 +118,23 @@ for m in range(0, int(Building_Num), 1):
         for i in trange(0, int(NUM) ,1):
             time.sleep(0.1)
             try:
+                # 사진 삽입
                 Image_Cycle = Input_Image(location_Col_1, location_Row_1, Path, Building, m, Image_Cycle)
                 Image_Cycle = Input_Image(location_Col_1, location_Row_2, Path, Building, m, Image_Cycle)
                 Image_Cycle = Input_Image(location_Col_1, location_Row_3, Path, Building, m, Image_Cycle)
+
+
             except:
                 print(str(Building[m]) + " " + str(Image_Cycle) + ".jpg 사진이 없습니다.")
                 break
+
+            # 엑셀 수식 삽입      
+            Combine_Explanation(location_Row_1,  location_Col_2)
+            Combine_Explanation(location_Row_2,  location_Col_2)
+            Combine_Explanation(location_Row_3,  location_Col_2)
+            Check_ImageNum(location_Row_1, location_Col_2)
+            Check_ImageNum(location_Row_2, location_Col_2)
+            Check_ImageNum(location_Row_3, location_Col_2)            
 
 
             location_Row_1 = Next_Location(location_Row_1)
